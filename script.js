8b60811a501372a2084be24fca1d6909e9b932e2
Icaro <icarodamian612@gmail.com>
2024-06-02 14:54:41 -0300

    mudei o portifolio

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const nav = document.getElementById('nav');
-
-    menuToggle.addEventListener('click', function() {
-        nav.classList.toggle('nav-open');
-    });
-});
